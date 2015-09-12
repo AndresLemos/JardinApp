5f5b4d1ea610e47167aa72b7a2d5cfad4c385938
root <root@rootbean>
2015-09-12 11:17:31 -0500

    se configuro la seguridad de glassfish, se creo el controller para login se configuro el web.xml para el control de grupos de usuarios  y se creo la vista y la base de datos llamada LoginView

--- JardinApp/src/java/com/example/jpa/entities/Roles.java
+++ JardinApp/src/java/com/example/jpa/entities/Roles.java
@@ -2,10 +2,12 @@ package com.example.jpa.entities;
 
 import java.io.Serializable;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
